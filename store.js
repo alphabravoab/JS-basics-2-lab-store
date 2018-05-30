1e482ef26c3b4aa8fdb1934ae882e01e89630220
Arjen <alphabravoab@gmail.com>
2018-05-30 09:37:53 +0200

    it works

--- store.js
+++ store.js
@@ -1,22 +1,61 @@
 // TODO:
 // var products = []
-var products = []
+let products = [
+ { referenceNumber: 1, name: "Super Lite Mat", price: 10 },
+ { referenceNumber: 2, name: "Power Mat", price: 20 },
+ { referenceNumber: 3, name: "Block", price: 30 },
+ { referenceNumber: 4, name: "Meditation cushion", price: 30 },
+ { referenceNumber: 5, name: "The best T-shirt", price: 200 },
+ { referenceNumber: 6, name: "The cutest yoga pants", price: 300 },
+ { referenceNumber: 7, name: "Bring Yoga To Life", price: 30 },
+ { referenceNumber: 8, name: "Light On Yoga", price: 10 }
+]
+
 // Declare `shoppingCart`, something where you will be storing all products that the user buys.
 // Declare `products`, the different that you will be selling under each of the departments.
-
-var shopFromStore =  function () {
-  var refNr = askUserForReferenceNumber();
+let shoppingCart=[];
+let totalprice=0;
+let shopFromStore = function () {
+  let refNr = parseInt(askUserForReferenceNumber());
   // Add the product with the matching referenceNumber to the shoppingCart
-
+  function findProduct(x){
+    console.log('function running');
+    return x.referenceNumber === refNr;
+  };
+  let gimmi = products.find(findProduct);
+  console.log(refNr);
+  console.log(gimmi);
+  shoppingCart.push(gimmi);
+  console.log(shoppingCart);
   displayProductsFromShoppingCart();
-
+  moreorstop();
   // calculate the total price of your cart, and use it:
-  displayTotalPrice(/*The variable holding the totol price*/);
 
-  // Ask the user if they want to continue shopping,
-  // if yes, call this function again
-  // if no, print the goodbye message
+
+  let Checkouts = shoppingCart.map(function(item) {
+    return item.price;
+});
+  let totalPrice = Checkouts.reduce(function(a,b){
+    return a+b;
+  });
+  console.log(totalprice);
+  displayTotalPrice(totalPrice);
 };
+
+// Ask the user if they want to continue shopping,
+// if yes, call this function again
+// if no, print the goodbye message
+let moreorstop = function(){
+let x = window.prompt( 'Do you want more junk?' )
+  if (x=='no'){
+    console.log('goodbye');
+  }else {
+    shopFromStore();
+  }
+}
+
+
+
 
 var displayProductsFromShoppingCart = function() {
   // iterate over the shoppingCart and display the contents
@@ -24,9 +63,9 @@ var displayProductsFromShoppingCart = function() {
   // use the printProductsOnScreen function for inspiration
 };
 
-var askUserForReferenceNumber = function() {
-  // Use window.prompt to ask the user a question and capture their response,
-  // then, return the response from this function back to our caller
+let askUserForReferenceNumber = function() {
+  return window.prompt('what product do you want')
+
 };
 
 //
